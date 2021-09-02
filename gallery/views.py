bcb8d5d40e27f3e4acfec23ee780cb1985589d12
Dennis Mwaniki <damos5776@gmail.com>
2021-09-02 21:53:02 +0300

    urlconf

--- gallery/views.py
+++ gallery/views.py
@@ -7,4 +7,4 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my personal gallery')
+    return render(request,'index.html')
