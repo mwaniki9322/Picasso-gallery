13d5ac1010f56c191f93e4860707d9876e6fbb07
Dennis Mwaniki <damos5776@gmail.com>
2021-09-02 23:56:46 +0300

    location model

--- gallery/models.py
+++ gallery/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Images(models.Model):
     image_name=models.CharField(max_length=70)
     description=models.TextField(max_length=2000)
+    
+    def __str__(self):
+        return self.image_name
+
+class Location(models.Model):
+    Location=models.TextField(max_length=100)
