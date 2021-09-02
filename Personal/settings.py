f1d613943bac8e38261c915f2027b63c7829d970
Dennis Mwaniki <damos5776@gmail.com>
2021-09-02 23:13:07 +0300

    database settings

--- Personal/settings.py
+++ Personal/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'Personal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'mwaniki',
+    'PASSWORD':'mwaniki',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
