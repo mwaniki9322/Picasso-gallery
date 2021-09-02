18d623f178a1605c6d347362358e0acbbea42874
Dennis Mwaniki <damos5776@gmail.com>
2021-09-02 23:35:14 +0300

    image model

--- gallery/models.py
+++ gallery/models.py
@@ -3,6 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Images(models.Model):
-    image = models.ImageField(upload_to = 'articles/',default='SOME STRING')
+    image_name=models.CharField(max_length=70)
     description=models.TextField(max_length=2000)
-    
