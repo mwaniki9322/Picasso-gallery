ade60104b84275d192b49c9b820f78cf3d5cfc6e
Dennis Mwaniki <damos5776@gmail.com>
2021-09-04 10:16:22 +0300

    location tests

--- gallery/models.py
+++ gallery/models.py
@@ -9,6 +9,10 @@ class Locations(models.Model):
     def __str__(self):
         return self.name
 
+    
+    def save_location(self):
+        self.save()
+
 class Category(models.Model):
     name=models.CharField(max_length=100)
 
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,20 @@
+from gallery.models import Locations
 from django.test import TestCase
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.nairobi= Locations(name='Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Locations))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.nairobi.save_location()
+        locations = Locations.objects.all()
+        self.assertTrue(len(locations) > 0)
