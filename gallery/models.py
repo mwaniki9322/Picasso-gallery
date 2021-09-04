2382ddf6e8d7a79f6c5f6c1af6f02773fc0de433
Dennis Mwaniki <damos5776@gmail.com>
2021-09-04 10:09:05 +0300

    db model

--- gallery/models.py
+++ gallery/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
+
 #create models here
 
-class Image(models.Model):
-    name = models.CharField(max_length =30)
-    description = models.CharField(max_length =300)
-
-    def __str__(self):
-            return self.name
-
-class locationss(models.Model):
-    name = models.CharField(max_length =30)
+class Locations(models.Model):
+    name=models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
 
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+     
+class Image(models.Model):
+    image_name=models.CharField(max_length=100)
+    image_description=models.TextField(max_length=300)
+    location=models.ForeignKey(Locations,on_delete=models.CASCADE)
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
 
