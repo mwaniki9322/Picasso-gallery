4f622ded462db91b3c19c465d23f35f90cd9fd90
Dennis Mwaniki <damos5776@gmail.com>
2021-09-04 10:58:06 +0300

    adding models to admin page

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
+from datetime import date
 from django.contrib import admin
+from . models import Image,Locations,Category
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Locations)
+admin.site.register(Category)
