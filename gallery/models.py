9534f2c4236f147b365751493b33488da588c865
Dennis Mwaniki <damos5776@gmail.com>
2021-09-04 09:12:08 +0300

    model image

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+#create models here
+
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.CharField(max_length =300)
+    
+
 
