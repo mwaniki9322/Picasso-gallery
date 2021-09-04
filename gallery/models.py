f7d7496f40ea644aab3b0838a1633b45651de018
Dennis Mwaniki <damos5776@gmail.com>
2021-09-04 10:19:44 +0300

    category tests

--- gallery/models.py
+++ gallery/models.py
@@ -13,11 +13,16 @@ class Locations(models.Model):
     def save_location(self):
         self.save()
 
+
+
 class Category(models.Model):
     name=models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
      
 class Image(models.Model):
     image_name=models.CharField(max_length=100)
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,4 +1,4 @@
-from gallery.models import Locations
+from gallery.models import Category, Locations
 from django.test import TestCase
 
 # Create your tests here.
@@ -18,3 +18,20 @@ class LocationTestClass(TestCase):
         self.nairobi.save_location()
         locations = Locations.objects.all()
         self.assertTrue(len(locations) > 0)
+
+#category tests
+class CategoryTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.hiking= Category(name='Hiking')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hiking,Category))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.nairobi.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
