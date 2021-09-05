5f3db97db912c6f00ddee08b9d6cbd19d71c5c37
Dennis Mwaniki <damos5776@gmail.com>
2021-09-05 08:03:10 +0300

    filter by location view function

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,7 +8,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('',views.welcome,name='welcome'),
     path('images/',views.images,name='images'),
-    path('search/',views.search_results,name='search_results')
+    path('search/',views.search_results,name='search_results'),
+    path('location/',views.image_location,name='imagelocation')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
